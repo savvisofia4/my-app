3a6f69a5cc0f85197e1033eb249da74d26e7be22
eiriniko <eiriniko@renvis.gr>
2022-10-10 19:01:18 +0300

    import app module to main.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesComponent } from './courses/courses.component';
 
-
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    CoursesComponent,
-    CourseComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CoursesComponent, CourseComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
-import {Point} from './point';
-let point = new Point(7,8);
+import { Point } from './point';
+import { AppModule } from './app/app.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+let point = new Point(7, 8);
 point.draw();
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
